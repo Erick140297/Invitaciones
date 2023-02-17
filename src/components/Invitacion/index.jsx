4337823eabdede2ffbf771cf46b08bcbcda56a24
Erick140297 <erickmonterrubio67@outlook.com>
2023-02-16 22:36:46 -0600

    invitacion

--- src/components/Invitacion/index.jsx
+++ src/components/Invitacion/index.jsx
@@ -7,9 +7,7 @@ const Invitación = () => {
       <h2 className={s.frase}>Invitación: </h2>
     
       <div className={s.text}>
-      <h3>En este día tan especial para nosotros nos gustaría que compartas nuestra felicidad asistiendo a nuestra boda.</h3>
-        {/* <p>Presentar invitación digital</p>
-        <p>para acceso al evento</p> */}
+      <h3 >En este día tan especial para nosotros nos gustaría que compartas nuestra felicidad asistiendo a nuestra boda.</h3>
       </div>
     </div>
   );
