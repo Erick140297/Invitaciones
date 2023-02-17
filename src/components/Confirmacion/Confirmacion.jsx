c41794767847056a5a61507d993fafa25b44ac38
Erick140297 <erickmonterrubio67@outlook.com>
2023-02-17 00:09:37 -0600

    invitacion

--- src/components/Confirmacion/Confirmacion.jsx
+++ src/components/Confirmacion/Confirmacion.jsx
@@ -6,7 +6,7 @@ import s from "./Confirmacion.module.css";
 const Confirmacion = () => {
   return (
     <div className={s.container}>
-      <h2>Confirmación </h2>
+      <h2>Confirmacia tu asistencia</h2>
       <a
         className={s.btnW}
         target="_blank"
@@ -16,11 +16,11 @@ const Confirmacion = () => {
         <span>Confirma por WhatsApp</span>
         <WhatsappIcon />
       </a>
-      <div>
-        <span>
-          Nos ayudaria mucho que nos recomiendes canciones para el evento.
-        </span>
-        <a
+      <div className={s.recomendacion}>
+        <h3>
+          Recomiendanos canciones para el evento.
+        </h3>
+        <a className={s.btnC}
           target="_blank"
           rel="noreferrer"
           href="https://forms.gle/6yMy9UrvLUkWw7YG6"
