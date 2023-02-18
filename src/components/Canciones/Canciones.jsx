f3b90fa92d433c4802d2ada117a96297f9fbecab
Erick140297 <erickmonterrubio67@outlook.com>
2023-02-18 17:42:11 -0600

    hoteles

--- src/components/Canciones/Canciones.jsx
+++ src/components/Canciones/Canciones.jsx
@@ -4,7 +4,7 @@ import s from "./Canciones.module.css";
 const Canciones = () => {
   return (
     <div className={s.container}>
-      <h2>¿Qué canciones no pueden faltar en la fiesta?</h2>
+      <h2 >¿Qué canciones no pueden faltar en la fiesta?</h2>
       <a
         className={s.btnC}
         target="_blank"
@@ -12,6 +12,16 @@ const Canciones = () => {
         href="https://docs.google.com/forms/d/11_91LGOhupqVopl15PMugKL9sEkt5zu5yBvBoxl-duY/edit?usp=drivesdk"
       >
         Recomendar
+      </a>
+      <br /><br />
+      <h2 >Sugerencias de regalos</h2>
+      <a
+        className={s.btnC}
+        target="_blank"
+        rel="noreferrer"
+        href="https://forms.gle/govZa3Jm8aKSaaZk9"
+      >
+        Sugerir
       </a>
     </div>
   );
