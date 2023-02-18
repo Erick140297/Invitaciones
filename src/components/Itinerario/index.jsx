6c2bc402477a8fdaa5fee70d1dbc4bb212be7576
Erick140297 <erickmonterrubio67@outlook.com>
2023-02-18 15:26:40 -0600

    hoteles

--- src/components/Itinerario/index.jsx
+++ src/components/Itinerario/index.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import s from "./Itinerario.module.css"
+import React from "react";
+import s from "./Itinerario.module.css";
 
 const Itinerario = () => {
-  return(
-    <div>
+  return (
+    <div className={s.container}>
       <h2>Itinerario</h2>
+      <h3>13:00 horas: Misa religiosa</h3>
+      <h3>16:00 horas: Recepción</h3>
+      <h3>16:30 horas: Boda Civil</h3>
+      <h3>17:30 horas: Banquete</h3>
+      <h3>18:30 horas: Inicio de baile</h3>
+      <h3>00:00 horas: Espectáculo sorpresa</h3>
     </div>
-  )
-}
+  );
+};
 
-export default Itinerario
+export default Itinerario;
