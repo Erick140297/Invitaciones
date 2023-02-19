4dea5b29be42a4fb29b3972953494034de7ec3f0
Erick140297 <erickmonterrubio67@outlook.com>
2023-02-19 17:10:23 -0600

    cancion

--- src/components/Portada/index.jsx
+++ src/components/Portada/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Countdown from "../Countdown";
 import s from "./Portada.module.css";
+import ReactAudioPlayer from "react-audio-player";
 
 const Portada = () => {
   return (
@@ -11,6 +12,9 @@ const Portada = () => {
         <h3>Sábado 22 de abril de 2023</h3>
       </div>
       <Countdown />
+      <br />
+      <ReactAudioPlayer src="https://res.cloudinary.com/dfaxzahb0/video/upload/v1676846653/audio/Nick_Cave_The_Bad_Seeds_-_Into_My_Arms_Official_Video_abrq9w.mp4" autoPlay controls />
+      <br />
     </div>
   );
 };
