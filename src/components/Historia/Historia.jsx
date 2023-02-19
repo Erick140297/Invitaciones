db57736ba378da18361146a54024319788040156
Erick140297 <erickmonterrubio67@outlook.com>
2023-02-18 20:45:50 -0600

    busqueda

--- src/components/Historia/Historia.jsx
+++ src/components/Historia/Historia.jsx
@@ -10,22 +10,8 @@ const Historia = () => {
         encontrado algo especial. Este tiempo compartido ha sido hermoso, y es
         apenas el principio de un nuevo viaje lleno de emociones"
       </h3>
-      <div className={s.fotos}>
-        <img className={s.foto}
-          src="https://res.cloudinary.com/dfaxzahb0/image/upload/v1676601148/Fotos%20Lili/20_la9qy7.jpg"
-          alt="1"
-        />
-        <img className={s.foto}
-          src="https://res.cloudinary.com/dfaxzahb0/image/upload/v1676601151/Fotos%20Lili/22_e9whis.jpg"
-          alt="2"
-        />
-        <img className={s.foto}
-          src="https://res.cloudinary.com/dfaxzahb0/image/upload/v1676601149/Fotos%20Lili/23_j4efps.jpg"
-          alt="3"
-        />
-      </div>
     </div>
   );
 };
 
-export default  Historia
+export default Historia;
