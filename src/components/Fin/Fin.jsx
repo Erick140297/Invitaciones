ce859cbebb76323ffcc7882a031da0ae1d475b84
Erick140297 <erickmonterrubio67@outlook.com>
2023-02-20 21:42:23 -0600

    negritas

--- src/components/Fin/Fin.jsx
+++ src/components/Fin/Fin.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import s from "./Fin.module.css"
+import s from "./Fin.module.css";
 
 export const Fin = () => {
   return (
     <div className={s.container}>
-      <h2>¡Los esperamos!</h2>
+      <h2>
+        <b>¡Los esperamos!</b>
+      </h2>
       <h2>
         "Cuando te das cuenta de que deseas pasar el resto de tu vida con
         alguien, quieres que el resto de tu vida empiece lo antes posible"
